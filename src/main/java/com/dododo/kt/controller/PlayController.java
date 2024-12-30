03bc0cd6e31ff6492d6263a948a8d33913b79773
dododo25 <dmytro.terekhov@outlook.com>
2024-12-30 16:44:14 +0200

    Fix an issue with a responsive layout

--- src/main/java/com/dododo/kt/controller/PlayController.java
+++ src/main/java/com/dododo/kt/controller/PlayController.java
@@ -31,10 +31,6 @@ public class PlayController {
                 .map(Integer.class::cast)
                 .orElse(null);
 
-        connected = true;
-        gameMode = GameMode.KARAOKE;
-        trackId = 7;
-
         if (!connected || gameMode == null || trackId == null) {
             return "redirect:/select";
         }
@@ -44,14 +40,13 @@ public class PlayController {
         session.setAttribute("answers", null);
         session.setAttribute("refreshed", false);
         session.setAttribute("refreshType", null);
-        session.setAttribute("deviceType", "pc");
 
         TrackGameMode mode = track.getModes().get(gameMode);
 
         model.addAttribute("label", String.format("\"%s - %s\" is now playing", track.getArtist(), track.getName()));
         model.addAttribute("src", mode.getSrc());
         model.addAttribute("text", mode.getText());
-        model.addAttribute("rawCommands", mode.getCommands().get(session.getAttribute("deviceType")));
+        model.addAttribute("rawCommands", mode.getCommands().get("pc"));
 
         return "play";
     }
